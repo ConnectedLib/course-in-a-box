4eb5d37c580ae88a5930de9807fe5a3f6b88af00
Kelly Hoffman <36975036+infobyway@users.noreply.github.com>
2022-07-13 16:44:06 -0400

    collapsibles

--- js/collapsible.js
+++ js/collapsible.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var collapsed-content = this.nextElementSibling;
+    if (collapsed-content.style.display === "block") {
+      collapsed-content.style.display = "none";
     } else {
-      content.style.display = "block";
+      collapsed-content.style.display = "block";
     }
   });
 }
